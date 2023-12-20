25a563a596de4ce7d38aa6c6e083241dc6449f78
mohansuri2002 <103757007+mohansuri2002@users.noreply.github.com>
2023-12-21 03:23:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Register from './components/Register';
+import Login from './components/Login';
+import AuthProvider from './context/auth';
+import Profile from './pages/Profile';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  return (
+    <>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/' element={<PrivateRoute Cmp={Home} />} />
+          <Route exact path='/profile' element={<PrivateRoute Cmp={Profile} />} />
+        </Routes>
+      </AuthProvider>
+    </>
+
+  );
+}
+
+export default App;
