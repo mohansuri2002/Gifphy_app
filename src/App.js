d01b98293c60921c565810b694ef2f6d0a1a980f
mohansuri2002 <103757007+mohansuri2002@users.noreply.github.com>
2023-12-21 09:55:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route exact path='/login' element={<Login />} />
           <Route exact path='/' element={<PrivateRoute Cmp={Home} />} />
           <Route exact path='/home' element={<PrivateRoute Cmp={Home} />} />
-          <Route exact path='/Giphy_app/' element={<PrivateRoute Cmp={Home} />} />
+          <Route exact path='/Gifphy_app' element={<PrivateRoute Cmp={Home} />} />
           <Route exact path='/profile' element={<PrivateRoute Cmp={Profile} />} />
         </Routes>
       </AuthProvider>
